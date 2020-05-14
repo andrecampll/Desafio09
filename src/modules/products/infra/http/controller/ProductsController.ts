fa2e0d9c0e197966fee4ac735e4387f17dd9b5e9
André Victor <andrevictor50@gmail.com>
2020-05-14 17:04:53 -0300

    🚀: finished second route

--- src/modules/products/infra/http/controller/ProductsController.ts
+++ src/modules/products/infra/http/controller/ProductsController.ts
@@ -1,10 +1,21 @@
 import { Request, Response } from 'express';
 
-import { container } from 'tsyringe';
 import CreateProductService from '@modules/products/services/CreateProductService';
+
+import { container } from 'tsyringe';
 
 export default class ProductsController {
   public async create(request: Request, response: Response): Promise<Response> {
-    // TODO
+    const { name, price, quantity } = request.body;
+
+    const createProducts = container.resolve(CreateProductService);
+
+    const product = await createProducts.execute({
+      name,
+      price,
+      quantity,
+    });
+
+    return response.json(product);
   }
 }
